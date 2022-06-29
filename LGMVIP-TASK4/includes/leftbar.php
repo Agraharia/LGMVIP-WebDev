0356bd41242f07edbdea51b61f3838c34435e2a8
Akash Agrahari <64489532+Agraharia@users.noreply.github.com>
2022-06-29 23:43:23 +0530

    inital commit

---
+++ LGMVIP-TASK4/includes/leftbar.php
@@ -0,0 +1,59 @@
+<div class="left-sidebar bg-black-300 box-shadow " style="background-color: Grey;">
+                        <div class="sidebar-content">
+                            <div class="user-info closed">
+                                <h6 class="title">Akash Agrahari</h6>
+                                <small class="info">Full-stack developer </small>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    
+                                    <li>
+                                        <a href="dashboard.php"> <span>DASHBOARD</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">TOOLS</span>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"> <span>Student Classes</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-class.php"><span>Create Class</span></a></li>
+                                            <li><a href="manage-classes.php"></i> <span>Manage Classes</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"> <span>Subjects</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-subject.php"> <span>Create Subject</span></a></li>
+                                            <li><a href="manage-subjects.php"></i> <span>Manage Subjects</span></a></li>
+                                           <li><a href="add-subjectcombination.php"> <span>Add Subject Combination </span></a></li>
+                                           <a href="manage-subjectcombination.php"></i> <span>Manage Subject Combination </span></a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"> <span>Students</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-students.php"> <span>Add Students</span></a></li>
+                                            <li><a href="manage-students.php"><span>Manage Students</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"> <span>Result</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-result.php"> <span>Add Result</span></a></li>
+                                            <li><a href="manage-results.php"> <span>Manage Result</span></a></li>
+                                           
+                                        </ul>
+                                        
+                                           
+                                    </li>
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+</div>
---
+++ LGMVIP-TASK4/includes/topbar.php
@@ -0,0 +1,44 @@
+  <nav class="navbar top-navbar bg-white box-shadow" style="background-color: black; ">
+            	<div class="container-fluid">
+                    <div class="row">
+                        <div class="navbar-header no-padding">
+                			<a class="navbar-brand" href="dashboard.php" style="color: white;">
+                			    STUDENT REGISTRATION MANAGEMENT System
+                			</a>
+                            <span class="small-nav-handle hidden-sm hidden-xs"><i class="fa fa-outdent" style="color: white;"></i></span>
+                			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+                				<span class="sr-only">Toggle navigation</span>
+                				<i class="fa fa-ellipsis-v" style="color: white;"></i>
+                			</button>
+                            <button type="button" class="navbar-toggle mobile-nav-toggle"  style="color: white;">
+                				<i class="fa fa-bars" style="color: white;"></i>
+                			</button>
+                		</div>
+                        <!-- /.navbar-header -->
+
+                		<div class="collapse navbar-collapse" id="navbar-collapse-1">
+                			<ul class="nav navbar-nav" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+                                <li class="hidden-sm hidden-xs"><a href="#" class="user-info-handle"><i class="fa fa-user"></i></a></li>
+                                <li class="hidden-sm hidden-xs"><a href="#" class="full-screen-handle"><i class="fa fa-arrows-alt"></i></a></li>
+                       
+                				<li class="hidden-xs hidden-xs"><!-- <a href="#">My Tasks</a> --></li>
+                               
+                			</ul>
+                            <!-- /.nav navbar-nav -->
+
+                			<ul class="nav navbar-nav navbar-right" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+                             
+                				
+                				    <li><a href="logout.php" class="color-danger text-center"><i class="fa fa-sign-out"></i> Logout</a></li>
+                					
+                		
+                            
+                			</ul>
+                            <!-- /.nav navbar-nav navbar-right -->
+                		</div>
+                		<!-- /.navbar-collapse -->
+                    </div>
+                    <!-- /.row -->
+            	</div>
+            	<!-- /.container-fluid -->
+            </nav>
